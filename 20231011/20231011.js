bad511943c68285f9c5ca859021d156bc8045ea7
admin <m0a220443d@edu.teu.ac.jp>
2023-10-11 17:49:52 +0900

    10/11

---
+++ 20231011/20231011.js
@@ -0,0 +1,11 @@
+let variableA = "最初の代入A";
+console.log(variableA);
+
+let variableB = "最初の代入B";
+console.log(variableB);
+
+variableA = "二回目の代入";
+console.log(variableA);
+
+variableA = variableB;
+console.log(variableA);
